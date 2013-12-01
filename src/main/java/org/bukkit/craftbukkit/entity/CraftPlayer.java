d267210a1d41b9b06ba7c7ce3402a63c67cafcc4
md_5 <md_5@live.com.au>
2013-12-01 18:18:41 +1100

    BungeeCord Support
    Provides support for IP forwarding via BungeeCord.

--- src/main/java/org/bukkit/craftbukkit/entity/CraftPlayer.java
+++ src/main/java/org/bukkit/craftbukkit/entity/CraftPlayer.java
@@ -1333,6 +1333,12 @@ public class CraftPlayer extends CraftHumanEntity implements Player {
     {
 
         @Override
+        public InetSocketAddress getRawAddress()
+        {
+            return (InetSocketAddress) getHandle().playerConnection.networkManager.getRawAddress();
+        }
+
+        @Override
         public boolean getCollidesWithEntities()
         {
             return getHandle().collidesWithEntities;
